67082b5c6276a30175cba87536074f9d8459e7cd
AK2001 <al.kelaiditis@gmail.com>
2019-02-22 21:52:10 +0200

    initiate commit

---
+++ ex.2.py
@@ -0,0 +1,16 @@
+#First Task
+a=int(input("give length of a square"))
+e1=a**2
+p1=a*4
+print('area is:' ,e1, 'and perimeter is:' ,p1)
+#Second Task
+b=int(input("give lenght for the base and the height of a triangle"))
+h=int(input())
+e2=(b*h)/2
+print('area is:',e2)
+#Third Task
+c=int(input("give ray length of a circle"))
+p=3.14
+e3=p*c**2
+p3=2*p*c
+print('area is:',e3,'perimeter is:',p3)
